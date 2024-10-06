0e453a14e74e34275824bc171b32f71716da18bc
Cony Choi <welcomechoi94@gmail.com>
2024-10-06 21:44:33 +0900

    store 메소드 주석 추가

--- src/lib/store.js
+++ src/lib/store.js
@@ -40,16 +40,28 @@ export default class Store {
     }
   }
 
-  // 목록 methods
+  /**
+   * 목록 Map 조회 메소드
+   * @returns
+   */
   getListMap() {
     return this.listMap;
   }
 
+  /**
+   * 단일 목록 생성 메소드
+   * @param {string} name - 목록 이름
+   * @param {*} todoList - 초기 할일 목록(optional)
+   */
   addListItem(name, todoList) {
     this.listMap.set(name, todoList ?? []);
     this.#saveStorage();
   }
 
+  /**
+   * 단일 목록 제거 메소드
+   * @param {string} name - 목록 이름
+   */
   deleteListItem(name) {
     this.listMap.delete(name);
 
@@ -59,26 +71,48 @@ export default class Store {
     this.#saveStorage();
   }
 
+  /**
+   * 선택된 목록 조회 메소드
+   * @returns
+   */
   getSelectedListItem() {
     return this.selectedListItem;
   }
 
+  /**
+   * 단일 목록 선택 메소드
+   * @param {string} name - 목록 이름
+   */
   setSelectedListItem(name) {
     this.selectedListItem = name;
     this.#saveStorage();
   }
 
-  // 할일 methods
+  /**
+   * 특정 목록에 대한 할일 목록 반환 메소드
+   * @param {string} name - 목록 이름
+   * @returns
+   */
   getTodoListByList(name) {
     return this.listMap.get(name);
   }
 
+  /**
+   * 특정 목록에 할일 추가 메소드
+   * @param {string} name - 목록 이름
+   * @param {*} todoItem - 할일 정보
+   */
   addTodoItemByList(name, todoItem) {
     const todoList = [...this.getTodoListByList(name), todoItem];
     this.listMap.set(name, todoList);
     this.#saveStorage();
   }
 
+  /**
+   * 특정 목록의 할일 제거 메소드
+   * @param {string} name - 목록 이름
+   * @param {number} todoItemIndex - 제거할 할 일 index
+   */
   deleteTodoItemByList(name, todoItemIndex) {
     const todoList = this.getTodoListByList(name).filter(
       (_, index) => index !== todoItemIndex
@@ -87,6 +121,12 @@ export default class Store {
     this.#saveStorage();
   }
 
+  /**
+   * 특정 목록의 할일 수정 메소드
+   * @param {string} name - 목록 이름
+   * @param {number} todoItemIndex - 수정할 할 일 index
+   * @param {*} todoItem - 수정된 할 일 정보
+   */
   updateTodoItemByList(name, todoItemIndex, todoItem) {
     const todoList = this.getTodoListByList(name);
     todoList[todoItemIndex] = todoItem;
@@ -94,10 +134,19 @@ export default class Store {
     this.#saveStorage();
   }
 
+  /**
+   * 포모도로 실행 중인 정보 반환 메소드
+   * @returns
+   */
   getPlayedTodoInfo() {
     return this.playedTodoInfo;
   }
 
+  /**
+   * 포모도로 실행 정보 업데이트 메소드
+   * @param {string} name - 목록 이름
+   * @param {number} todoItemIndex - 할일 index
+   */
   setPlayedTodoInfo(name, todoItemIndex) {
     this.playedTodoInfo = {
       listName: name,
@@ -106,6 +155,9 @@ export default class Store {
     this.#saveStorage();
   }
 
+  /**
+   * 포모도로 실행 제거 메소드
+   */
   deletePlayedTodoInfo() {
     this.playedTodoInfo = null;
     this.#saveStorage();
